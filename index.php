c5bdd4269d1cb87c6a4a1a96440183ed23ed1f42
Ouches <53819265+Flesktech@users.noreply.github.com>
2020-07-04 03:03:19 +0100

    Updated index

--- index.php
+++ index.php
@@ -5,12 +5,28 @@ $serviceCode = $_POST["serviceCode"];
 $text = $_POST["text"];
 //This is the first menu screen
 if ( $text == "" ) {
-$response  = "CON Hi welcome, I can help you with Event Reservation \n";
-$response .= "1. Enter 1 to continue";
+$response  = "Welcome to Ouches Technology \n";
+$response .= "1. Enter 1 to About us";
+$response .= "2. Enter 2 to Check Our Products";
+$response .= "3. Enter 3 to Services";
 }
 // Menu for a user who selects '1' from the first menu
 // Will be brought to this second menu screen
 else if ($text == "1") {
+$response  = "About Us \n";
+$response .= "1. Contact information \n";
+$response .= "2. Goals and Objective \n";
+$response .= "3. Who we are \n";
+$response .= "4. Support \n";
+}
+else if ($text == "2") {
+$response  = "CON  Pick a table for reservation below \n";
+$response .= "1. Table for 2 \n";
+$response .= "2. Table for 4 \n";
+$response .= "3. Table for 6 \n";
+$response .= "4. Table for 8 \n";
+}
+else if ($text == "3") {
 $response  = "CON  Pick a table for reservation below \n";
 $response .= "1. Table for 2 \n";
 $response .= "2. Table for 4 \n";
